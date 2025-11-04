58c544cb27ab4b0883d0e854225e4fa9efc55114
Abdullah Bukhari <abdullah.bukhari@emumba.com>
2025-11-04 19:18:37 +0500

    test: add jest-dom for improved assertions in SearchBar tests

--- components/__tests__/SearchBar.test.tsx
+++ components/__tests__/SearchBar.test.tsx
@@ -27,6 +27,7 @@
 
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import { SearchBar } from '../SearchBar';
 
 // ============================================================================
